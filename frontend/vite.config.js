1a52c8849e8a7a99e5f8ca88c36ac333261e1d12
sandeeprepala <sandeeprepala3@gmail.com>
2025-06-17 14:10:20 +0530

    Added vercel.json

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import nodePolyfills from 'rollup-plugin-node-polyfills';
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/',
   server: {
   proxy: {
     '/api': 'http://localhost:8000',
